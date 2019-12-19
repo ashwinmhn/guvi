100daaf7797b01902020ec36b8697a901becb2e7
Ashwin <ashwinmhn@users.noreply.github.com>
2019-12-20 02:14:56 +0530

    Add files via upload

---
+++ loop/callist.py
@@ -0,0 +1,17 @@
+#program to print all Perfect numbers between 1 to n.
+
+print("enter N")
+a=int(input())
+k=0 
+sum=0 
+i = 1
+while i<a:
+  while k<a
+   if (k%i) == 0:
+    sum+=i  
+   i = i + 1
+if(a==sum):
+ print(k)
+
+else:
+ print("Number is not a perfect number")    
