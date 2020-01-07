2f7b42e1d03b3c32fbe21780e15eca139a20c25d
Ashwin <ashwinmhn@users.noreply.github.com>
2020-01-07 10:40:04 +0530

    Update problem4.ts

--- Typescript htask/problem4.ts
+++ Typescript htask/problem4.ts
@@ -1,12 +1,11 @@
-let arr = ["GUVI", "I", "am", "a geek"];
-let newobjt={"1":"2"};
-
-
-for (var keys in newobjt)
-{
-    keys=arr[0];
-    newobjt[keys]=arr[3];
+var arr = ["GUVI", "I", "am", "a geek"];
+var newobjt = { "1": "2" };
+function transformFirstAndlast(arr) {
+    for (var keys in newobjt) {
+        keys = arr[0];
+        newobjt[keys] = arr[3];
+    }
 }
-
-
+transformFirstAndlast(arr);
+delete newobjt["1"];
 console.log(newobjt);
